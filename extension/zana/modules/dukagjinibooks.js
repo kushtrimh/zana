ade7cdd9fee9de59d53ad4ba9ff31d3aa73e5e8a
kushtrimh <kushtrimmh13@gmail.com>
2023-04-29 13:58:47 +0200

    refactor(extension): convert star creation from plain html to objects

--- extension/zana/modules/dukagjinibooks.js
+++ extension/zana/modules/dukagjinibooks.js
@@ -204,20 +204,44 @@ dukagjini.init = function () {
         let filledStarPath = 'M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z'
         let emptyStarPath = 'M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z';
 
+        // Loop through the rating and create the star elements
         for (let i = 1; i <= 5; i++) {
+            // Determine the star path based on the rating
             const starPath = i <= rating ? filledStarPath : emptyStarPath;
-            starsElement.insertAdjacentHTML('beforeend', `
-            <span class="b-rating-star">
-                <span class="b-rating-icon">
-                    <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="star" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-star b-icon bi">
-                        <g>
-                            <path d="${starPath}">
-                            </path>
-                        </g>
-                    </svg>
-                </span>
-            </span>
-        `)
+
+            // Create the star element
+            const starElement = document.createElement('span');
+            starElement.classList.add('b-rating-star', 'dukagjinibooks-star');
+
+            const starIcon = document.createElement('span');
+            starIcon.className = 'b-rating-icon';
+
+            const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            svgElement.setAttribute('viewBox', '0 0 16 16');
+            svgElement.setAttribute('width', '1em');
+            svgElement.setAttribute('height', '1em');
+            svgElement.setAttribute('focusable', 'false');
+            svgElement.setAttribute('role', 'img');
+            svgElement.setAttribute('aria-label', 'star');
+            svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+            svgElement.setAttribute('fill', 'currentColor');
+            svgElement.classList.add('bi-star', 'b-icon', 'bi');
+
+            const gElement = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+
+            const pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+            pathElement.setAttribute('d', starPath);
+
+            // Add the path element to the group element, and the group element to the SVG element
+            gElement.appendChild(pathElement);
+            svgElement.appendChild(gElement);
+
+            // Add the SVG element to the icon element, and the icon element to the star element
+            starIcon.appendChild(svgElement);
+            starElement.appendChild(starIcon);
+
+            // Add the star element to the array
+            starsElement.append(starElement);
         }
         return starsElement;
     }
