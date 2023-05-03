8959707440f0fd4ef12098192429801a15586e88
kushtrimh <kushtrimmh13@gmail.com>
2023-05-03 18:06:33 +0200

    refactor: clean up old build directories for Chrome build

--- tools/extension/chrome_build.sh
+++ tools/extension/chrome_build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-mkdir -p dist.chrome.mv3.build
+rm -rf dist.chrome.mv3.build
+rm -rf dist.chrome.mv3.release
+
+mkdir dist.chrome.mv3.build
 cp -r extension/addon/* dist.chrome.mv3.build
 rm dist.chrome.mv3.build/manifest.json
 cp extension/platform/chrome/manifest.json dist.chrome.mv3.build
+
 if [ "$1" = "--release" ]; then
     mkdir -p dist.chrome.mv3.release
     zip -r dist.chrome.mv3.release/extension.zip dist.chrome.mv3.build
